f1f870af99a782cb93f63d8801d74e5797a14fd1
Rail <railbolo@gmail.com>
2022-05-20 19:19:25 +0400

    ДЗ1

---
+++ lesson1_homework/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!--<HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <HelloWorld :msg="msg" />
+    <p>{{ msg }}</p>
+    <button @click="inc(5, $event)">{{ counter }}</button>
+		<Calc />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Calc from "./components/Calc.vue";
+
+
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "Hello Rail",
+      counter: 0,
+    };
+  },
+  methods: {
+    inc(step, event) {
+      console.log(event);
+      this.counter+=step;
+    },
+  },
+  components: {
+    HelloWorld,
+		Calc,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lesson1_homework/src/components/Calc.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <div class="main">
+      <div class="screen">
+        <input type="number" v-model.number="op1" />
+        <input type="number" v-model.number="op2" />
+        <div class="equ">=</div>
+        <div class="res">{{ result }}</div>
+      </div>
+      <div class="calc">
+        <button @click="add">+</button>
+        <button @click="sub">-</button>
+        <button @click="mult">*</button>
+        <button @click="div">/</button>
+        <button @click="pow">x<sup>y</sup></button>
+        <button @click="intDiv">DIV</button>
+      </div>
+      <div class="clear">
+        <button @click="reset">RESET</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calc",
+  data() {
+    return {
+      op1: 0,
+      op2: 0,
+      result: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.result = this.op1 + this.op2;
+    },
+    sub() {
+      this.result = this.op1 - this.op2;
+    },
+    mult() {
+      this.result = this.op1 * this.op2;
+    },
+    div() {
+      this.result = (this.op1 / this.op2).toFixed(2);
+    },
+    pow() {
+      this.result = Math.pow(this.op1, this.op2);
+    },
+    intDiv() {
+      this.result = Math.floor(this.op1 / this.op2);
+    },
+    reset() {
+      this.result = 0;
+      this.op1 = 0;
+      this.op2 = 0;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.main {
+  border: 2px solid black;
+  display: flex;
+  flex-direction: column;
+  margin: 50px auto;
+  width: 400px;
+  height: 300px;
+  .screen {
+    margin: 20px auto;
+    display: flex;
+    justify-content: center;
+    width: 300px;
+    input {
+      text-align: center;
+      margin: 15px;
+      width: 50px;
+      height: 30px;
+    }
+    .equ {
+      margin-top: 20px;
+      margin-right: 15px;
+      width: 20px;
+      height: 20px;
+    }
+    .res {
+      margin-top: 15px;
+      text-align: center;
+      padding-top: 5px;
+      width: 60px;
+      height: 30px;
+      border: 1px solid black;
+    }
+  }
+  .calc {
+    margin: 30px auto;
+    width: 200px;
+    display: flex;
+    flex-wrap: wrap;
+    button {
+      display: block;
+      width: 50px;
+      height: 35px;
+      margin: 5px;
+    }
+  }
+  .clear {
+    height: 40px;
+  }
+}
+</style>
